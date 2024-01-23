e03e10b85ba4e6886fa03d5de3873ced5b436f87
KarimovaFatima <karimovafatima127@gmail.com>
2024-01-23 22:00:15 +0400

    intial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// ! TASK 1<<<<< yaza bilmedimmm(((
+
+// let date = "1k22k2024";
+// let dateSplit= date.split("k");
+
+// function changeOperator (date){
+//     if(date!=dateSplit.join("/")){
+//         date.split().join("/")
+//     }
+//     let result = dateSplit.join("/");
+//     console.log("🚀 ~ changeOperator ~ result:", result)
+// };
+// changeOperator(date);
+
+
+// ! TASK 2<<<<<<<
+// let workers = [
+//     {
+//         name: "Farid", salary: 5000
+//     },
+//     {
+//         name: "Elvin", salary: 1000
+//     },
+//     {
+//         name: "Murad", salary: 1000
+//     },
+//     {
+//         name: "Ruslan", salary: 1000
+//     },
+// ];
+
+// let totalSalary = workers.reduce((total,value)=> total + value.salary,0);
+// console.log("🚀 ~ totalSalary:", totalSalary);
+
+
+
+// ! TASK 3<<<<<<<
+
+// let arr1 = [1, 2, 3, 4, 5, 6];
+// let arr2 = [1, 2, 3, 4, 5,];
+// let extraElements = findExtraElements(arr1, arr2);
+
+// function findExtraElements(arr1, arr2){
+//     return arr1.filter(function(extra){
+//       return !arr2.includes(extra)});
+//   };
+
+//  console.log("🚀 ~ extraElements:", extraElements);
+  
+  
